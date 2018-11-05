4b9ff7ffb9945db9e2b075959df324baa895be8e
keaukraine <keaukraine@gmail.com>
2018-11-05 21:55:29 +0200

    Fixed blending with background issues.

--- js/app/framework/BaseRenderer.js
+++ js/app/framework/BaseRenderer.js
@@ -137,7 +137,7 @@ define([
             var gl = null;
 
             try {
-                gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+                gl = canvas.getContext('webgl', { alpha: false }) || canvas.getContext('experimental-webgl', { alpha: false });
                 gl.viewportWidth = canvas.width;
                 gl.viewportHeight = canvas.height;
                 this.isETC1Supported = !!gl.getExtension('WEBGL_compressed_texture_etc1');
